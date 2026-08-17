51fbcec84ee91a8bcb381c1eb113a5ed9b39020c
Miguel Fraga <miguel.fraga.pico@udc.es>
2026-08-17 15:08:35 +0200

    modified tools

--- release.bat
+++ release.bat
@@ -1,52 +1,110 @@
 @echo off
-SET WINRAR="C:\Program Files\WinRAR\rar.exe"
-SET MOD_FOLDER="%LOCALAPPDATA%\plutonium\storage\t6\mods\zm_strattester"
+setlocal
 
-XCOPY ".\localized\st_watermark.str" ".\english\localizedstrings\st_watermark.str" /Y
+SET "WINRAR=C:\Program Files\WinRAR\WinRAR.exe"
+SET "RELEASE_DIR=release"
 
-echo Compiling english version...
+SET "EN_NAME=Strat Tester BO2"
+SET "ES_NAME=Strat Tester BO2 Espanol"
+SET "PT_NAME=Strat Tester BO2 PT-BR"
+SET "GL_NAME=Strat Tester BO2 Galego"
+
+if not exist "%RELEASE_DIR%" mkdir "%RELEASE_DIR%"
+
+if exist "%RELEASE_DIR%\mod.iwd" del /Q "%RELEASE_DIR%\mod.iwd"
+
+"%WINRAR%" a -afzip -r "%RELEASE_DIR%\mod.iwd" "ui\*"
+
+if exist "%RELEASE_DIR%\%EN_NAME%" rmdir /S /Q "%RELEASE_DIR%\%EN_NAME%"
+mkdir "%RELEASE_DIR%\%EN_NAME%\zm_strattester"
+
+XCOPY "%RELEASE_DIR%\mod.iwd" "%RELEASE_DIR%\%EN_NAME%\zm_strattester\" /Y
+XCOPY "mod.json" "%RELEASE_DIR%\%EN_NAME%\zm_strattester\" /Y
+XCOPY "docs\scriptdata" "%RELEASE_DIR%\%EN_NAME%\zm_strattester\scriptdata" /E /I /Y
 
 XCOPY ".\localized\en\st_hud.str" ".\english\localizedstrings\st_hud.str" /Y
 XCOPY ".\localized\en\st_menu.str" ".\english\localizedstrings\st_menu.str" /Y
 XCOPY ".\localized\en\st_perks.str" ".\english\localizedstrings\st_perks.str" /Y
 XCOPY ".\localized\en\zone.str" ".\english\localizedstrings\zone.str" /Y
 
-call builder
+cmd /c builder
 
-%WINRAR% a -ep1 "release\Strat Tester BO2.rar" "%LOCALAPPDATA%\plutonium\storage\t6\mods\zm_strattester"
+XCOPY "zone\mod.ff" "%RELEASE_DIR%\%EN_NAME%\zm_strattester\" /Y
 
-echo Compiling spanish version...
+pushd "%RELEASE_DIR%\%EN_NAME%"
+"%WINRAR%" a -r "..\%EN_NAME%.rar" "zm_strattester"
+popd
+
+rmdir /S /Q "%RELEASE_DIR%\%EN_NAME%"
+
+:: --- VERSIÓN EN ESPAÑOL ---
+if exist "%RELEASE_DIR%\%ES_NAME%" rmdir /S /Q "%RELEASE_DIR%\%ES_NAME%"
+mkdir "%RELEASE_DIR%\%ES_NAME%\zm_strattester"
+
+XCOPY "%RELEASE_DIR%\mod.iwd" "%RELEASE_DIR%\%ES_NAME%\zm_strattester\" /Y
+XCOPY "mod.json" "%RELEASE_DIR%\%ES_NAME%\zm_strattester\" /Y
+XCOPY "docs\scriptdata" "%RELEASE_DIR%\%ES_NAME%\zm_strattester\scriptdata" /E /I /Y
 
 XCOPY ".\localized\es\st_hud.str" ".\english\localizedstrings\st_hud.str" /Y
 XCOPY ".\localized\es\st_menu.str" ".\english\localizedstrings\st_menu.str" /Y
 XCOPY ".\localized\es\st_perks.str" ".\english\localizedstrings\st_perks.str" /Y
 XCOPY ".\localized\es\zone.str" ".\english\localizedstrings\zone.str" /Y
 
-call builder
+cmd /c builder
 
-%WINRAR% a -ep1 "release\Strat Tester BO2 Espanol.rar" "%LOCALAPPDATA%\plutonium\storage\t6\mods\zm_strattester"
+XCOPY "zone\mod.ff" "%RELEASE_DIR%\%ES_NAME%\zm_strattester\" /Y
 
-echo Compiling portuguese version...
+pushd "%RELEASE_DIR%\%ES_NAME%"
+"%WINRAR%" a -r "..\%ES_NAME%.rar" "zm_strattester"
+popd
+
+rmdir /S /Q "%RELEASE_DIR%\%ES_NAME%"
+
+:: --- VERSIÓN EN PORTUGUÉS ---
+if exist "%RELEASE_DIR%\%PT_NAME%" rmdir /S /Q "%RELEASE_DIR%\%PT_NAME%"
+mkdir "%RELEASE_DIR%\%PT_NAME%\zm_strattester"
+
+XCOPY "%RELEASE_DIR%\mod.iwd" "%RELEASE_DIR%\%PT_NAME%\zm_strattester\" /Y
+XCOPY "mod.json" "%RELEASE_DIR%\%PT_NAME%\zm_strattester\" /Y
+XCOPY "docs\scriptdata" "%RELEASE_DIR%\%PT_NAME%\zm_strattester\scriptdata" /E /I /Y
 
 XCOPY ".\localized\pt\st_hud.str" ".\english\localizedstrings\st_hud.str" /Y
 XCOPY ".\localized\pt\st_menu.str" ".\english\localizedstrings\st_menu.str" /Y
 XCOPY ".\localized\pt\st_perks.str" ".\english\localizedstrings\st_perks.str" /Y
 XCOPY ".\localized\pt\zone.str" ".\english\localizedstrings\zone.str" /Y
 
-call builder
+cmd /c builder
 
-%WINRAR% a -ep1 "release\Strat Tester BO2 PT-BR.rar" "%LOCALAPPDATA%\plutonium\storage\t6\mods\zm_strattester"
+XCOPY "zone\mod.ff" "%RELEASE_DIR%\%PT_NAME%\zm_strattester\" /Y
 
-echo Compiling galician version...
+pushd "%RELEASE_DIR%\%PT_NAME%"
+"%WINRAR%" a -r "..\%PT_NAME%.rar" "zm_strattester"
+popd
+
+rmdir /S /Q "%RELEASE_DIR%\%PT_NAME%"
+
+:: --- VERSIÓN EN GALEGO ---
+if exist "%RELEASE_DIR%\%GL_NAME%" rmdir /S /Q "%RELEASE_DIR%\%GL_NAME%"
+mkdir "%RELEASE_DIR%\%GL_NAME%\zm_strattester"
+
+XCOPY "%RELEASE_DIR%\mod.iwd" "%RELEASE_DIR%\%GL_NAME%\zm_strattester\" /Y
+XCOPY "mod.json" "%RELEASE_DIR%\%GL_NAME%\zm_strattester\" /Y
+XCOPY "docs\scriptdata" "%RELEASE_DIR%\%GL_NAME%\zm_strattester\scriptdata" /E /I /Y
 
 XCOPY ".\localized\gl\st_hud.str" ".\english\localizedstrings\st_hud.str" /Y
 XCOPY ".\localized\gl\st_menu.str" ".\english\localizedstrings\st_menu.str" /Y
 XCOPY ".\localized\gl\st_perks.str" ".\english\localizedstrings\st_perks.str" /Y
 XCOPY ".\localized\gl\zone.str" ".\english\localizedstrings\zone.str" /Y
 
-call builder
+cmd /c builder
 
-%WINRAR% a -ep1 "release\Strat Tester BO2 Galego.rar" "%LOCALAPPDATA%\plutonium\storage\t6\mods\zm_strattester"
+XCOPY "zone\mod.ff" "%RELEASE_DIR%\%GL_NAME%\zm_strattester\" /Y
 
+pushd "%RELEASE_DIR%\%GL_NAME%"
+"%WINRAR%" a -r "..\%GL_NAME%.rar" "zm_strattester"
+popd
 
-echo Release completed!
+rmdir /S /Q "%RELEASE_DIR%\%GL_NAME%"
+del /Q "%RELEASE_DIR%\mod.iwd"
+
+endlocal
